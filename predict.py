e7c17f21604b8299d6f1b77f24683206c49dea29
Rob Chiocchio <1686473+RobChiocchio@users.noreply.github.com>
2026-09-17 10:40:01 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,6 +1,8 @@
 import gradio as gr
 
-from transformers import WhisperForAudioClassification, WhisperProcessor, WhisperFeatureExtractor
+#from transformers import WhisperForAudioClassification, WhisperProcessor, WhisperFeatureExtractor
+from transformers import AutoModelForAudioClassification, AutoFeatureExtractor, Wav2Vec2ForSequenceClassification
+from peft import PeftModel
 
 # import torch
 
@@ -9,16 +11,51 @@ from transformers import WhisperForAudioClassification, WhisperProcessor, Whispe
 
 from transformers import pipeline
 
-model_id = "RobChio/swissgerman-dialect-classifier"
-base_model_id = "Flix-AI/flix-swissgerman-full"
+adapter_id = "RobChio/swissgerman-dialect-classifier-tiny"
+#base_model_id = "Flix-AI/flix-swissgerman-full"
+base_model_id = "openai/whisper-tiny"
+# adapter_id = "RobChio/swissgerman-dialect-classifier-xls-r"
+# base_model_id = "facebook/wav2vec2-xls-r-300m"
 
-model = WhisperForAudioClassification.from_pretrained(model_id)
-feature_extractor = WhisperFeatureExtractor.from_pretrained(base_model_id)
-processor = WhisperProcessor.from_pretrained(base_model_id)
+# model = WhisperForAudioClassification.from_pretrained(model_id)
+# feature_extractor = WhisperFeatureExtractor.from_pretrained(base_model_id)
+# processor = WhisperProcessor.from_pretrained(base_model_id)
 
 # processor = WhisperProcessor.from_pretrained(model_id) # set to original
 # model = WhisperForAudioClassification.from_pretrained(model_id)
 
+# model = AutoModelForAudioClassification.from_pretrained(model_id)
+
+labels = {
+    0: "AG",
+    1: "BE",
+    2: "BS",
+    3: "GR",
+    4: "LU",
+    5: "SG",
+    6: "VS",
+    7: "ZH",
+}
+
+id2label = labels
+label2id = {v: k for k, v in labels.items()}
+
+base_model = AutoModelForAudioClassification.from_pretrained(
+    base_model_id,
+    torch_dtype="auto",
+    num_labels=8,
+    label2id=label2id,
+    id2label=id2label,
+)
+
+peft_model = PeftModel.from_pretrained(
+    base_model,
+    adapter_id,
+)
+
+model = peft_model.merge_and_unload()
+
+feature_extractor = AutoFeatureExtractor.from_pretrained(base_model_id)
 
 # Load a pre-trained audio classification model pipeline
 pipe = pipeline("audio-classification", model=model, feature_extractor=feature_extractor)
@@ -32,10 +69,10 @@ def classify_audio(filepath):
 demo = gr.Interface(
     fn=classify_audio,
     inputs=gr.Audio(type="filepath", label="Upload or Record Audio"),
-    outputs=gr.Label(num_top_classes=3, label="Predictions"),
+    outputs=gr.Label(num_top_classes=8, label="Predictions"),
     title="Audio Classification with Transformers",
     description="Upload an audio file to classify its content using a fine-tuned transformer model."
 )
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(share=False)
